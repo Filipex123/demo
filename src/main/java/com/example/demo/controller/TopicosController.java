6c3950faed8641d82b535e65ca42b74bfc9c9d4f
FilipeZ <filipe_zanelato@hotmail.com>
2019-10-27 22:06:02 -0300

    y

--- src/main/java/com/example/demo/controller/TopicosController.java
+++ src/main/java/com/example/demo/controller/TopicosController.java
@@ -1,31 +1,42 @@
 package com.example.demo.controller;
 
+import com.example.demo.controller.form.TopicoForm;
 import com.example.demo.dto.TopicoDTO;
 import com.example.demo.model.Curso;
 import com.example.demo.model.Topico;
+import com.example.demo.repository.CursoRepository;
 import com.example.demo.repository.TopicoRepository;
 import com.sun.javafx.util.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 @Controller
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     TopicoRepository topicoRepository;
 
-    @GetMapping("/topicos")
-    @ResponseBody()
+    @Autowired
+    CursoRepository cursoRepository;
+
+    @GetMapping()
     public List<TopicoDTO> listaTopicos(String nomeCurso){
         List<Topico> lista = (Objects.isNull(nomeCurso))?topicoRepository.findAll():topicoRepository.findByCursoNome(nomeCurso);
         return TopicoDTO.converter(lista);
+    }
+
+    @PostMapping()
+    public ResponseEntity<> cadastrar(@RequestBody TopicoForm form){
+    Topico topico = form.converter(cursoRepository);
+    topicoRepository.save(topico);
     }
 }
